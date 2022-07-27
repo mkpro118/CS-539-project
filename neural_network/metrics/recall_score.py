e0360b23d03480cea5e2cb7ac9e1fd8f60bf714b
catalinasandoval <asandoval4@wisc.edu>
2022-07-27 17:30:41 -0500

    updated recall_score (:

--- neural_network/metrics/recall_score.py
+++ neural_network/metrics/recall_score.py
@@ -1,1 +1,26 @@
-# To be implemented
+import numpy as np
+
+def overallRecall(cmat):
+    '''
+    Calculates overall recall score of the model
+
+    Parameters: 
+        cmat: confusion matrix (2d: n_labels, n_labels)
+    
+    Returns: 
+        float: recall score
+    '''
+    rows, cols = cmat.shape
+
+    tp = np.trace(cmat)
+    sumRows = 0
+
+    # get tp+fn values for each label
+    for i in range (rows):
+        for j in range (cols):
+            if (j >= i):
+                sumRows += cmat[i, j]
+
+    recall = tp / sumRows
+
+    return recall
