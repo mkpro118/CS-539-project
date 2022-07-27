234dc575014c05d57d23bef8508a695098c067ae
catalinasandoval <asandoval4@wisc.edu>
2022-07-27 17:01:03 -0500

    accuracy updated

--- neural_network/metrics/accuracy.py
+++ neural_network/metrics/accuracy.py
@@ -1,1 +1,18 @@
 # To be implemented
+import numpy as np
+from sklearn import metrics
+
+def overallAccuracy(cmat, K):
+    '''
+    Calculates overall accuracy of the model
+
+    Parameters: 
+        cmat: confusion matrix (n_labels, n_labels)
+        K: total amount of samples
+    
+    Returns: 
+        float: 0 <= accuracy of the model <= 1
+    '''
+    accuracy = float(np.trace(cmat)/(K))
+
+    return accuracy
