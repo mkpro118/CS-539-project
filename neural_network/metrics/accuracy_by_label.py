cddc57ebe4994663954c3b2d6cd90d0804a2908d
M K <mkpro118@gmail.com>
2022-07-30 02:34:12 +0530

    Refactored, added parameter validation

--- neural_network/metrics/accuracy_by_label.py
+++ neural_network/metrics/accuracy_by_label.py
@@ -1,14 +1,21 @@
 import numpy as np
 
+from ..exceptions import ExceptionFactory
 from ..utils.typesafety import type_safe, not_none
 from ..utils.exports import export
 from .confusion_matrix import confusion_matrix
 
 
+errors = {
+    'AccuracyByLabelError': ExceptionFactory.register('AccuracyByLabelError'),
+}
+
+
 @type_safe
 @not_none
 @export
-def accuracy_by_label(y_true: np.ndarray, y_pred: np.ndarray, *, normalize: bool = True) -> np.ndarray:
+def accuracy_by_label(y_true: np.ndarray, y_pred: np.ndarray, *,
+                      normalize: bool = True) -> np.ndarray:
     '''
     Calculates accuracy of the model by label
 
@@ -27,6 +34,11 @@ def accuracy_by_label(y_true: np.ndarray, y_pred: np.ndarray, *, normalize: bool
                     if normalize is false, it's the number of
                     correct classifications under that label
     '''
+    if y_true.shape != y_pred.shape:
+        raise errors['AccuracyByLabelError'](
+            f'y_true and y_pred must have the same shapes, '
+            f'{y_true.shape} != {y_pred.shape}'
+        )
     return (
         (
             correct := np.diagonal(
