d0291f92a339fc8d2ba155d3c5d96b7ada298b3b
M K <mkpro118@gmail.com>
2022-07-30 01:28:44 +0530

    Refactored, added docs, typesafety and export

--- neural_network/metrics/accuracy_by_label.py
+++ neural_network/metrics/accuracy_by_label.py
@@ -1,41 +1,31 @@
-# will finish cleaning up
 import numpy as np
 
+from ..utils.typesafety import type_safe, not_none
+from ..utils.exports import export
+from .confusion_matrix import confusion_matrix
 
-def accuracy_by_label(cmat):
+
+@type_safe
+@not_none
+@export
+def accuracy_by_label(y_true: np.ndarray, y_pred: np.ndarray, *, normalize: bool = True) -> np.ndarray:
     '''
     Calculates accuracy of the model by label
 
     Parameters:
-        cmat: confusion matrix (2d array: n_labels, n_labels)
+        y_true: np.ndarray of shape (n_samples, n_classes)
+            known one hot encoded labels
+        y_pred: np.ndarray of shape (n_samples, n_classes)
+            predicted one hot encoded labels
 
     Returns:
-        1d float array: each element of the array has an accuracy between
-        0 and 1, corresponding with it's label position in the confusion
-        matrix
+        np.ndarray: each element of the array is the normalized accuracy of that label
+                    if normalize is false, it's the number of correct classifications under that label
     '''
-    rows, cols = cmat.shape
-
-    accuracy = np.zeros(rows, dtype=float)
-    truePositive = np.zeros(rows, dtype=float)
-    rowSum = np.zeros(rows, dtype=float)
-    colSum = np.zeros(rows, dtype=float)
-
-    diagSum = np.trace(cmat)
-
-    # get row sum
-    for i in range(rows):
-        truePositive[i] = cmat[i, i]
-        for j in range(cols):
-            rowSum[i] += cmat[i, j]
-
-    # get col sum
-    for i in range(cols):
-        for j in range(rows):
-            colSum[i] += cmat[j, i]
-
-    # get accuracy
-    for i in range(rows):
-        accuracy[i] = float(diagSum / ((rowSum[i] - truePositive[i]) + (colSum[i] - truePositive[i]) + diagSum))
-
-    return accuracy
+    return (
+        (
+            correct := np.diagonal(
+                (cmat := confusion_matrix(y_true, y_pred))
+            )
+        ) / np.sum(cmat, axis=0)
+    ) if normalize else correct
