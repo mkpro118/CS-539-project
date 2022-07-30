97b047304d29f96255efe6a9e83030e95196d0db
M K <mkpro118@gmail.com>
2022-07-31 01:07:57 +0530

    Added support for binary labels, TODO: multi-label

--- neural_network/metrics/precision_score.py
+++ neural_network/metrics/precision_score.py
@@ -1,28 +1,105 @@
 import numpy as np
-from sklearn import metrics
+
+from ..exceptions import ExceptionFactory
+from ..utils.typesafety import type_safe, not_none
+from ..utils.exports import export
+from .confusion_matrix import confusion_matrix
+
+errors = {
+    'PrecisionScoreError': ExceptionFactory.register('PrecisionScoreError'),
+}
 
 
-def precision_score(cmat):
+@type_safe
+@not_none
+def _score_binary_label(cmat: np.ndarray) -> float:
+    tp, _, fp, _ = cmat.ravel()
+    return tp / (tp + fp)
+
+
+'''TODO below'''
+
+
+@type_safe
+@not_none
+def _score_multi_label_micro(cmat: np.ndarray) -> float:
+    pass
+
+
+@type_safe
+@not_none
+def _score_multi_label_macro(cmat: np.ndarray) -> float:
+    pass
+
+
+'''TODO above'''
+
+_scoring_functions = {
+    'binary': _score_binary_label,
+    'micro': _score_multi_label_micro,
+    'macro': _score_multi_label_macro,
+}
+
+
+@type_safe
+@not_none
+@export
+def precision_score(y_true: np.ndarray, y_pred: np.ndarray, *,
+                    average: str = 'binary') -> float:
     '''
-    Calculates overall precision score of the model
+    Calculates the precision score of the model
+
+    For binary classifications, it is defined as
+    Precision = True Positives / (True Positives + False Positives)
+
+    For multi-label classifications, it is computed in two ways
+        micro-averaged:
+            It is computed globally over all labels at once,
+            giving equal weight to each decision. Tends to be dominated
+            by the classifier's performance on the more common labels
+
+            It is computes using the formula
+                                    Sum of True Positives
+            Precision = ----------------------------------------------
+                        Sum of True Positives + Sum of False Positives
+            where all sums are by label
+
+        macro-averaged:
+            It is computed categorically (by label), giving equal weight
+            to each label. Tends to be dominated by classifier's performance
+            on rarer labels
+
+            It is computes using the formula
+                        Sum of precisions of each label
+            Precision = ------------------------------
+                               Number of labels
+
 
     Parameters:
-        cmat: confusion matrix (2d: n_labels, n_labels)
+        y_true: np.ndarray of shape (n_samples, n_classes)
+            known one hot encoded labels
+        y_pred: np.ndarray of shape (n_samples, n_classes)
+            predicted one hot encoded labels
 
     Returns:
-        float: precision score
+        float: The precision score
     '''
-    rows, cols = cmat.shape
+    if y_true.shape != y_pred.shape:
+        raise errors['PrecisionScoreError'](
+            f'y_true and y_pred must have the same shapes, '
+            f'{y_true.shape} != {y_pred.shape}'
+        )
 
-    tp = np.trace(cmat)
-    sumCols = 0
+    if average not in ['binary', 'micro', 'macro']:
+        raise errors['PrecisionScoreError'](
+            'average must be one of [\'binary\', \'micro\', \'macro\']'
+        )
 
-    # get tp+fp values for each label
-    for i in range(cols):
-        for j in range(rows):
-            if (j >= i):
-                sumCols += cmat[j, i]
+    cmat = confusion_matrix(y_true, y_pred)
+    if cmat.shape[-1] != 2 and average == 'binary':
+        raise errors['PrecisionScoreError'](
+            f'y_true has been detected to contain multiple labels, but {average=}. '
+            f'Set the scoring setting from one of [\'micro\', \'macro\']'
+        )
 
-    precision = tp / sumCols
-
-    return precision
+    return float(_scoring_functions[average](cmat))
