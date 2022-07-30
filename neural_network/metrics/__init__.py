2950fdc19940b52db701f2e134a94cdb4e1709ce
M K <mkpro118@gmail.com>
2022-07-31 01:00:50 +0530

    Removed deleted functions

--- neural_network/metrics/__init__.py
+++ neural_network/metrics/__init__.py
@@ -2,8 +2,6 @@
 
 from .accuracy_by_label import accuracy_by_label
 from .accuracy_score import accuracy_score
-from .average_precision_score import average_precision_score
-from .average_recall_score import average_recall_score
 from .confusion_matrix import confusion_matrix
 from .precision_score import precision_score
 from .recall_score import recall_score
