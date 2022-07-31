2d157b72bb291bb1bf2947a16513b3dc4999798f
M K <mkpro118@gmail.com>
2022-08-01 02:41:31 +0530

    Added __name_to_symbol_map__

--- neural_network/activation/__init__.py
+++ neural_network/activation/__init__.py
@@ -1,7 +1,15 @@
 '''Allow classes to be accesed from package level imports'''
 
-from .leaky_relu import LeakyReLU
-from .relu import ReLU
-from .sigmoid import Sigmoid
-from .softmax import Softmax
-from .tanh import Tanh
+from .leaky_relu import LeakyReLU, __name_to_symbol_map__ as leaky_relu_symbol_map
+from .relu import ReLU, __name_to_symbol_map__ as relu_symbol_map
+from .sigmoid import Sigmoid, __name_to_symbol_map__ as sigmoid_symbol_map
+from .softmax import Softmax, __name_to_symbol_map__ as softmax_symbol_map
+from .tanh import Tanh, __name_to_symbol_map__ as tanh_symbol_map
+
+__name_to_symbol_map__ = {
+    **leaky_relu_symbol_map,
+    **relu_symbol_map,
+    **sigmoid_symbol_map,
+    **softmax_symbol_map,
+    **tanh_symbol_map,
+}
