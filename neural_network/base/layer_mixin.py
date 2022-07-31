cc4dea3975683bd42063d5d13401623992c36790
M K <mkpro118@gmail.com>
2022-08-01 02:45:01 +0530

    Added Layer Mixin, base class for all layers

--- neural_network/base/layer_mixin.py
+++ neural_network/base/layer_mixin.py
@@ -1,9 +1,72 @@
+from typing import Union, Callable
+from numbers import Integral, Real
+import numpy as np
+from functools import wraps
+
 from .mixin import mixin
+from .activation_mixin import ActivationMixin
 from .metadata_mixin import MetadataMixin
+from .save_mixin import SaveMixin
+
+from ..activation import __name_to_symbol_map__ as symbol_map
+from ..exceptions import ExceptionFactory
+from ..preprocess import Scaler
+from ..utils.typesafety import type_safe, not_none
+
+activation_symbol_map = {name.lower(): symbol for name, symbol in symbol_map.items()}
+
+errors = {
+    'WeightInitializationError': ExceptionFactory.register('WeightInitializationError'),
+    'OptimizationError': ExceptionFactory.register('OptimizationError'),
+    'InvalidInvocationError': ExceptionFactory.register('InvalidInvocationError'),
+    'InvalidConstraintsError': ExceptionFactory.register('InvalidConstraintsError'),
+    'InvalidActivationError': ExceptionFactory.register('InvalidActivationError'),
+}
+
+
+class MethodInvalidator:
+    invalid_methods = set()
+
+    @staticmethod
+    @type_safe
+    @not_none
+    def register(func: Callable):
+        MethodInvalidator.invalid_methods.add(func.__qualname__)
+
+    @staticmethod
+    @type_safe
+    @not_none
+    def validate(func: Callable):
+        if func.__qualname__ in MethodInvalidator.invalid_methods:
+            MethodInvalidator.invalid_methods.remove(func.__qualname__)
+
+    @staticmethod
+    @type_safe
+    @not_none
+    def is_invocable(func: Callable) -> bool:
+        return func.__qualname__ not in MethodInvalidator.invalid_methods
+
+    @staticmethod
+    @type_safe
+    @not_none(nullable=('invalid_logic',))
+    def check_validity(func: Callable = None, *, invalid_logic: Callable = None) -> Callable:
+        def decorator(func):
+            @wraps(func)
+            def inner(self, *args, **kwargs):
+                if not MethodInvalidator.is_invocable(func):
+                    return invalid_logic(self, *args, **kwargs)
+                return func(self, *args, **kwargs)
+            return inner
+
+        if invalid_logic is None:
+            invalid_logic = lambda *_, **__: None
+            return decorator(func)
+        else:
+            return decorator
 
 
 @mixin  # Prevents instantiation
-class LayerMixin(MetadataMixin):
+class LayerMixin(MetadataMixin, SaveMixin):
     '''
     Mixin class for all Layers.
 
@@ -13,8 +76,168 @@ class LayerMixin(MetadataMixin):
         method `get_metadata` to computer layer's metadata
     '''
 
-    def __str__(self):
-        return f'{self.__class__} Layer'
+    @type_safe
+    @not_none
+    def __init__(self, *, activation: Union[str, ActivationMixin] = None,
+                 use_bias: bool = True,
+                 trainable: bool = True,
+                 weights_constraints: Union[np.ndarray, list, tuple] = None,
+                 bias_constraints: Union[np.ndarray, list, tuple] = None):
+        self.activation = activation
+        self.use_bias = use_bias
+        self._trainable = trainable
+        self.weights_constraints = weights_constraints
+        self.bias_constraints = bias_constraints
 
+        self._check_activation()
+        self._check_constraints()
+
+        self._rng = np.random.default_rng()
+
+    @type_safe
+    @not_none
+    def _check_activation(self):
+        if self.activation is None:
+            return MethodInvalidator.register(self.apply_activation)
+
+        if isinstance(self.activation, ActivationMixin):
+            return
+
+        try:
+            self.activation = activation_symbol_map[self.activation.replace('_', '').lower()]
+            print('KeyError')
+        except KeyError:
+            raise errors['InvalidActivationError'](
+                f'activation={self.activation} is not a recognized activation function'
+            )
+
+    @MethodInvalidator.check_validity(invalid_logic=lambda self, X: X)
+    @type_safe
+    def apply_activation(self, X: np.ndarray) -> np.ndarray:
+        return self.activation.apply(X)
+
+    @type_safe
+    def _check_constraints(self):
+        if not self.use_bias:
+            MethodInvalidator.register(self._check_bias_constraints)
+            MethodInvalidator.register(self.ensure_bias_constraints)
+
+        self._check_weights_constraints()
+        self._check_bias_constraints()
+
+    @type_safe
+    def _check_weights_constraints(self):
+        if self.weights_constraints is None:
+            return MethodInvalidator.register(self.ensure_weight_constraints)
+
+        constraint = np.asarray(self.weights_constraints)
+
+        if constraint.ndim != 1 or constraint.shape[-1] != 2:
+            raise errors['InvalidConstraintsError'](
+                f'weights_constraints parameter must be 1 dimensional with the first index being the '
+                f'lower bound of the weights, and the second index being the upper '
+                f'bound of the weights, found {constraint=}'
+            )
+
+    @MethodInvalidator.check_validity
+    def _check_bias_constraints(self):
+        print('checking')
+        if self.bias_constraints is None:
+            return MethodInvalidator.register(self.ensure_bias_constraints)
+
+        constraint = np.asarray(self.bias_constraints)
+
+        if constraint.ndim != 1 or constraint.shape[-1] != 2:
+            raise errors['InvalidConstraintsError'](
+                f'bias_constraints parameter must be 1 dimensional with the first index being the '
+                f'lower bound of the bias, and the second index being the upper '
+                f'bound of the bias, found {constraint=}'
+            )
+
+    @MethodInvalidator.check_validity
+    def ensure_weight_constraints(self):
+        np.clip(
+            self.weights,
+            self.weights_constraints[0],
+            self.weights_constraints[1],
+            out=self.weights
+        )
+
+    @MethodInvalidator.check_validity
+    def ensure_bias_constraints(self):
+        np.clip(
+            self.bias,
+            self.bias_constraints[0],
+            self.bias_constraints[1],
+            out=self.bias
+        )
+
+    def optimize(self, *args, **kwargs):
+        if self.trainable:
+            raise errors['OptimizationError'](
+                f'optimization logic is not implemented'
+            )
+
+    @type_safe
+    @not_none(nullable=('scale',))
+    def generate_weights(self, shape: Union[np.ndarray, list, tuple], *,
+                         mean: Union[np.floating, np.integer, float, Real, int, Integral] = None,
+                         std: Union[np.floating, np.integer, float, Real, int, Integral] = None,
+                         scale: Union[np.ndarray, list, tuple, Scaler] = None,
+                         from_rng: np.random.Generator = None) -> np.ndarray:
+        _shape = tuple(shape)
+
+        _mean = mean if mean else 0.0
+        _std = std if std else 1.0
+
+        if from_rng:
+            _weights = from_rng.normal(loc=_mean, scale=_std, size=_shape)
+        else:
+            _weights = self._rng.normal(loc=_mean, scale=_std, size=_shape)
+
+        if not scale:
+            return _weights
+
+        if (mean or std) and scale:
+            raise errors['WeightInitializationError'](
+                f'The weights cannot be scaled if mean or standard deviation is specified'
+            )
+
+        if isinstance(scale, Scaler):
+            return scale.fit_transform(_weights)
+
+        try:
+            return Scaler(scale[0], scale[1]).fit_transform(_weights)
+        except IndexError:
+            raise errors['WeightInitializationError'](
+                f'scale parameter must be 1 dimensional with the first index being the '
+                f'lower bound of the scaled values, and the second index being the upper '
+                f'bound of the scaled values, found {scale=}'
+            )
+
+    @property
+    @type_safe
+    def trainable(self) -> bool:
+        return self._trainable
+
+    @trainable.setter
+    @type_safe
+    @not_none
+    def trainable(self, value: bool):
+        self._trainable = value
+        optimizer = self.__dict__.get('optimizer', None)
+        if optimizer is None:
+            return
+
+        if self.trainable:
+            MethodInvalidator.validate(optimizer)
+        else:
+            MethodInvalidator.register(optimizer)
+
+    @type_safe
+    def __str__(self):
+        return f'{self.__class__.__name__} Layer'
+
+    @type_safe
     def __repr__(self):
         return str(self)
