d266991585f744fe32432ffb2386cb1da4da62f1
M K <mkpro118@gmail.com>
2022-07-31 22:03:10 +0530

    added package level import for multilabel_confusion_matrix

--- neural_network/metrics/__init__.py
+++ neural_network/metrics/__init__.py
@@ -2,6 +2,6 @@
 
 from .accuracy_by_label import accuracy_by_label
 from .accuracy_score import accuracy_score
-from .confusion_matrix import confusion_matrix
+from .confusion_matrix import confusion_matrix, multilabel_confusion_matrix
 from .precision_score import precision_score
 from .recall_score import recall_score
