7cc92411e42d64df7ba162ab0b50fbf41899e380
M K <mkpro118@gmail.com>
2022-08-07 03:59:50 +0530

    Improved implementation

--- data/grayscale.py
+++ data/grayscale.py
@@ -4,10 +4,10 @@ from concurrent.futures import ProcessPoolExecutor
 
 
 def process(args):
-    original, resized, name = args
+    original, grayscaled, name = args
     img = Image.open(os.path.join(original, name)).copy()
     img = img.convert('L')
-    img.save(os.path.join(resized, name))
+    img.save(os.path.join(grayscaled, name))
 
 
 def all_(args):
@@ -18,33 +18,37 @@ def all_(args):
             pass
 
 
-if __name__ == '__main__':
-    b_root = 'images/original/bishop/'
-    k_root = 'images/original/knight/'
-    p_root = 'images/original/pawn/'
-    q_root = 'images/original/queen/'
-    r_root = 'images/original/rook/'
+def grayscale():
+    original_folders = (
+        'images/original/bishop/',
+        'images/original/knight/',
+        'images/original/pawn/',
+        'images/original/queen/',
+        'images/original/rook/',
+    )
 
-    b_new = 'images/resized/bishop/'
-    k_new = 'images/resized/knight/'
-    p_new = 'images/resized/pawn/'
-    q_new = 'images/resized/queen/'
-    r_new = 'images/resized/rook/'
+    new_folders = (
+        'images/grayscaled/bishop/',
+        'images/grayscaled/knight/',
+        'images/grayscaled/pawn/',
+        'images/grayscaled/queen/',
+        'images/grayscaled/rook/',
+    )
 
-    b_files = next(os.walk(b_root))[2]
-    k_files = next(os.walk(k_root))[2]
-    p_files = next(os.walk(p_root))[2]
-    q_files = next(os.walk(q_root))[2]
-    r_files = next(os.walk(r_root))[2]
+    for folder in original_folders:
+        assert os.path.isdir(folder)
+
+    for new_folder in new_folders:
+        if not os.path.isdir(new_folder):
+            os.makedirs(new_folder)
+
+    files = tuple((next(os.walk(folder))[2] for folder in original_folders))
+
     with ProcessPoolExecutor() as executor:
-        results = executor.map(
-            all_, (
-                (b_files, b_root, b_new),
-                (k_files, k_root, k_new),
-                (p_files, p_root, p_new),
-                (q_files, q_root, q_new),
-                (r_files, r_root, r_new),
-            )
-        )
+        results = executor.map(all_, (zip(files, original_folders, new_folders)))
         for result in results:
             pass
+
+
+if __name__ == '__main__':
+    grayscale()
