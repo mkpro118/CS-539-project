8da35af602b68b907e1658d05346c54c153c23a6
M K <mkpro118@gmail.com>
2022-08-08 00:41:26 +0530

    Refactored for python3.6 compatibility

--- neural_network/__init__.py
+++ neural_network/__init__.py
@@ -3,3 +3,7 @@ import sys
 if sys.version_info < (3, 6, 0):
     print("neural_network requires python3 version >= 3.6.0", file=sys.stderr)
     sys.exit(1)
+elif sys.version_info < (3, 8, 0):
+    import warnings
+    x, y, z = sys.version_info[:3]
+    warnings.warn('typing support is not compatible with python version ' + '.'.join((x, y, z)))
