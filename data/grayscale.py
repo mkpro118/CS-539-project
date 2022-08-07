15cdcffd7c7960797424ba02dca369f2004b9a88
M K <mkpro118@gmail.com>
2022-08-07 18:48:22 +0530

    Added comments and docs

--- data/grayscale.py
+++ data/grayscale.py
@@ -1,16 +1,45 @@
+'''
+Utility functions to grayscale all images in the 'images/original/' folder
+'''
+
 import os
 from PIL import Image
 from concurrent.futures import ProcessPoolExecutor
 
 
 def process(args):
+    '''
+    Grayscales an image, and saves them to the corresponding folder
+
+    Parameters:
+        args: tuple
+            The tuple of the original folder, new folder, filename
+
+    Note: Takes in a single argument instead of 3
+          to allow the usage of ProcessPoolExecutor
+    '''
+    # unpack args
     original, grayscaled, name = args
-    img = Image.open(os.path.join(original, name)).copy()
-    img = img.convert('L')
+
+    # grayscale
+    img = Image.open(os.path.join(original, name)).convert('L')
+
+    # save
     img.save(os.path.join(grayscaled, name))
 
 
 def all_(args):
+    '''
+    Grayscales all images in the given subfolder
+
+    Parameters:
+        args: tuple
+            The tuple of the original filename, original folder, new folder
+
+    Note: Takes in a single argument instead of 3
+          to allow the usage of ProcessPoolExecutor
+    '''
+
     files, _root, _new = args
     with ProcessPoolExecutor() as executor:
         results = executor.map(process, ((_root, _new, i) for i in files))
@@ -19,6 +48,10 @@ def all_(args):
 
 
 def grayscale():
+    '''
+    Primary function in this module, grayscales all images found in 'images/original/'
+    '''
+    # tuple of original image folders
     original_folders = (
         'images/original/bishop/',
         'images/original/knight/',
@@ -27,6 +60,7 @@ def grayscale():
         'images/original/rook/',
     )
 
+    # tuple of grayscaled image folders
     new_folders = (
         'images/grayscaled/bishop/',
         'images/grayscaled/knight/',
@@ -35,15 +69,19 @@ def grayscale():
         'images/grayscaled/rook/',
     )
 
+    # Ensure original folders exist
     for folder in original_folders:
         assert os.path.isdir(folder)
 
+    # Create new folders if needed
     for new_folder in new_folders:
         if not os.path.isdir(new_folder):
             os.makedirs(new_folder)
 
+    # get tuple of all files
     files = tuple((next(os.walk(folder))[2] for folder in original_folders))
 
+    # grayscale with multiprocessing to speed things up
     with ProcessPoolExecutor() as executor:
         results = executor.map(all_, (zip(files, original_folders, new_folders)))
         for result in results:
--- data/resize.py
+++ data/resize.py
@@ -1,18 +1,47 @@
+'''
+Utility functions to resize all images in the 'images/original/' folder
+'''
+
 import os
 from PIL import Image
 from concurrent.futures import ProcessPoolExecutor
 
-width, height = 224, 244
+# The width and height to resize to
+width, height = 100, 100
 
 
 def process(args):
+    '''
+    Resizes an image, and saves them to the corresponding folder
+
+    Parameters:
+        args: tuple
+            The tuple of the original folder, new folder, filename
+
+    Note: Takes in a single argument instead of 3
+          to allow the usage of ProcessPoolExecutor
+    '''
+    # unpack args
     original, resized, name = args
-    img = Image.open(os.path.join(original, name)).copy()
-    img = img.resize((width, height))
+
+    # resize
+    img = Image.open(os.path.join(original, name)).resize((width, height))
+
+    # save
     img.save(os.path.join(resized, name))
 
 
 def all_(args):
+    '''
+    Resizes all images in the given subfolder
+
+    Parameters:
+        args: tuple
+            The tuple of the original filename, original folder, new folder
+
+    Note: Takes in a single argument instead of 3
+          to allow the usage of ProcessPoolExecutor
+    '''
     files, _root, _new = args
     with ProcessPoolExecutor() as executor:
         results = executor.map(process, ((_root, _new, i) for i in files))
@@ -21,10 +50,14 @@ def all_(args):
 
 
 def resize(shape):
-    nonlocal width, height
+    '''
+    Primary function in this module, resizes all images found in 'images/original/'
+    '''
+    global width, height
 
     width, height = shape
 
+    # tuple of original image folders
     original_folders = (
         'images/original/bishop/',
         'images/original/knight/',
@@ -33,6 +66,7 @@ def resize(shape):
         'images/original/rook/',
     )
 
+    # tuple of resized image folders
     new_folders = (
         'images/resized/bishop/',
         'images/resized/knight/',
@@ -41,15 +75,19 @@ def resize(shape):
         'images/resized/rook/',
     )
 
+    # Ensure original folders exist
     for folder in original_folders:
         assert os.path.isdir(folder)
 
+    # Create new folders if needed
     for new_folder in new_folders:
         if not os.path.isdir(new_folder):
             os.makedirs(new_folder)
 
+    # get tuple of all files
     files = tuple((next(os.walk(folder))[2] for folder in original_folders))
 
+    # resize with multiprocessing to speed things up
     with ProcessPoolExecutor() as executor:
         results = executor.map(all_, (zip(files, original_folders, new_folders)))
         for result in results:
@@ -57,4 +95,4 @@ def resize(shape):
 
 
 if __name__ == '__main__':
-    resize(width, height)
+    resize((width, height))
